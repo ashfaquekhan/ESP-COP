09d7d316a94de2bdf7922e41dd7b570deff10054
Ashfaque Khan <42895491+ashfaquekhan@users.noreply.github.com>
2024-10-08 15:40:58 +0530

    tuning applied

--- main/main.cpp
+++ main/main.cpp
@@ -49,8 +49,8 @@ float dt;
 float ax, ay, az, gx, gy, gz;
 bool clamp = true;
         //P: 0.1 | I:0.0003 | D:0.08 
-float rKp=0.09,rKi=0.0001,rKd=0.07; 
-float pKp=0.09,pKi=0.0001,pKd=0.07;
+float rKp=0.1,rKi=0.0003,rKd=0.08; 
+float pKp=0.1,pKi=0.0003,pKd=0.08;
 float yKp=0.1,yKi=0.0009,yKd=0.0;
 
 float errR,errP,errY;
